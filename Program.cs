009bbcce42a17e4f15ee4740af3e041d4502531a
quockhanh22 <anhkhanh11761@gmail.com>
2020-09-08 22:38:40 +0700

    kiemtranamnhuan
    kiemtranamnhuan

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace kiemtranamnhuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            int years;
+            Console.Write("Nhập năm muốn kiểm tra : ");
+            years = Convert.ToInt32(Console.ReadLine());
+
+            if (years % 400 == 0 && years % 100 == 0)
+            {
+                Console.Write("Năm {0} là năm Nhuận ", years);
+            }
+            else
+            {
+                Console.Write("Năm {0} không phải là năm Nhuận ", years);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
